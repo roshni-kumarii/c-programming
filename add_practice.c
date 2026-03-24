5b6655aace7f0f3306141e72412750712a195471
roshni <roshnikumari@proton.me>
2026-03-24 20:01:17 +0530

    add two number

---
+++ add_practice.c
@@ -0,0 +1,38 @@
+//loops ?
+// ki loops hota kya hai???
+// kitna types ka loops hai??
+// loops -> while,for,do while
+
+//while loop
+
+
+
+// add 2 no.s taking user inputs
+
+//add 2 no.s --> user input
+// -> 2 variable lenge -> num1,num2
+//-> ek variable aur lenge ...kyu?? -> kyuki add karne ke baad isko store karenge uss variable me
+// -> add -> ye ke varibale hai
+
+//ab baat karte hai user input pe
+//userinput lene ke liye kya use karte hai????---> scanf
+//then print--> aa gaya output
+
+
+#include<stdio.h>
+int main()
+{
+	int sum, a,b;
+	
+	printf("enter 1st number:");
+	scanf("%d",&a);
+	
+	printf("\nenter 2nd number:");
+	scanf("%d",&b);
+	
+	
+	sum=a+b;
+	printf("total sum is = %d",sum);
+	
+	
+}
